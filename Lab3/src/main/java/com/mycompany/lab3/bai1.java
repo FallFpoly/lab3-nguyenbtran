218242eeb26b5b9fc30f078a5bca86eb6da3aac5
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-09-30 20:19:10 +0700

    Lab3_bai1

--- Lab3/src/main/java/com/mycompany/lab3/bai1.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai1.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 public class bai1 {
 
     public static void main(String[] args) {
+        System.out.print("Nguyễn Phạm Bảo Trân");
          Scanner sc = new Scanner(System.in);
         System.out.print("Nhap so can kiem tra ");
         int N = sc.nextInt();
